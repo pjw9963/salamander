b150d88ed771d4a9673b9f8307198205913e47f7
Boxfish <metakirby123@gmail.com>
2019-11-01 19:23:39 -0400

    Add sendmsg route for sending messages to the chatbot

--- salamander/chatbot/views.py
+++ salamander/chatbot/views.py
@@ -1,6 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
     return render(request, 'chatbot/index.html')
+
+@csrf_exempt
+def sendmsg(request):
+    #Reject anything that's not a POST request
+    if request.method != "POST":
+        errMsg = {
+            "error": "bruh"
+        }
+        return JsonResponse(errMsg)
+    #Handle message
+    msg = request.POST["msg"]
+    print(msg)
+    data = {
+        "response": "bruh"
+    }
+    return JsonResponse(data)
