019da1a9fd609fb397027148a73d6d7aaae622c8
pjw9963 <pjw9963@rit.edu>
2019-11-01 18:12:56 -0400

    added static folder

--- salamander/chatbot/models.py
+++ salamander/chatbot/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=256)
     ratings = models.IntegerField()
-    value = models.DecimalField(decimal_places=3, max_digits=4)
+    value = models.IntegerField()
 
