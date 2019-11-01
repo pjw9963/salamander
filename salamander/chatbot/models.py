bbc76eea525cfbd538844c8b7c82481280e25dab
pjw9963 <pjw9963@rit.edu>
2019-11-01 19:30:59 -0400

    added base html file

--- salamander/chatbot/models.py
+++ salamander/chatbot/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Movie(models.Model):
-    title = models.CharField(max_length=256)
+    title = models.CharField(max_length=100)
     ratings = models.IntegerField()
     value = models.IntegerField()
+    movie_id = models.IntegerField()
 
