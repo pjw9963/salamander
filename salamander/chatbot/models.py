ae80fe94b8a45cfff21ce4481866a18a827f3c00
Owen Racette <ojr4316@rit.edu>
2019-11-01 20:18:44 -0400

    heres some more

--- salamander/chatbot/models.py
+++ salamander/chatbot/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
-    ratings = models.IntegerField()
-    value = models.IntegerField()
-    movie_id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    total = models.IntegerField()
+    sum = models.IntegerField()
+    id = models.IntegerField(primary_key=True)
 
--- salamander/chatbot/views.py
+++ salamander/chatbot/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from . import models as models
+
 
 def index(request):
+    print(models.Movie.objects)
     return render(request, 'chatbot/index.html')
